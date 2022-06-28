7e0d0f3d9cb4583fada85d0c1b604cbdf4909c39
Orllewin <orllewin@fastmail.uk>
2022-06-28 11:34:06 +0100

    add torment font

--- Source/main.lua
+++ Source/main.lua
@@ -10,6 +10,9 @@ assert(fontBBCMicroMode5)
 local fontBBCMicroMode5Large = playdate.graphics.font.new("fonts/font_bbc_mode_5_large")
 assert(fontBBCMicroMode5Large)
 
+local fontTorment = playdate.graphics.font.new("fonts/font_torment")
+assert(fontTorment)
+
 function playdate.update()	
 	fontBBCMicroMode1:drawText('Mode 1: Hello World', 10, 10)
 	fontBBCMicroMode1Large:drawText('Mode 1 Large: Hello World', 10, 30)
@@ -17,4 +20,6 @@ function playdate.update()
 	fontBBCMicroMode5:drawText('Mode 5: Hello World', 10, 60)
 	fontBBCMicroMode5Large:drawText('Mode 5 Large: Hello World', 10, 80)
 	fontBBCMicroMode5Large:drawText('Hello World', 10, 100)
+	
+	fontTorment:drawText('Torment: Hello World', 10, 130)
 end
